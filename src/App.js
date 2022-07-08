72454112ca886b8e66bad9d999ceb747b9ea829e
backbenchrs <meetlenwala912@gmail.com>
2022-07-08 11:03:26 +0530

    08/07/2022

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // let x=10;
+  // x=x+2;
+  // console.log(x); //12
+
+  // const x=10;
+  // x=5;
+  // console.log(x); // error
+
+    // let x=5;
+    // x=5;
+    // const x;
+    // x=5;
+    // console.log(x); //error
+
+  // const x=5; // Global Scope
+  // {
+  //   const x=5; // Local Scope
+  // }
+  // console.log(x);
+
+  // const display =(Name , id) => {
+  //   console.log(Name , id);
+  // }
+  // display("M" , 11); 
+
+
+
+  // Array 
+
+  let grid =[10,20,30,40,50,60,"m"];
+  console.log(grid[6] , grid);
   return (
     <div className="App">
       <header className="App-header">
