6d1c0fa422a949a7765cdd6f70f5027627716ef1
urmiljoshi <urmiljoshi16@gmail.com>
2022-07-11 12:55:08 +0530

    11th_july_2022

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
 
-  let arr=[10,50,80,44,48,36,];
-  let arr1=[39,88];
+  // let arr=[10,50,80,44,48,36];
+  // let arr1=[39,88]; 
 
   // let newArr=arr.concat(arr1);
   // console.log(newArr); //10,50,80,44,48,36 length:8
@@ -15,12 +15,19 @@ function App() {
 //  console.log(arr.tostring());  
 
   // let ans=arr.find((a)=> a>20);
-  // console.log(ans); //50
+  // console.log(ans); //50    
 
 
   // arr.splice(1,0,66); //10, 66, 50, 80, 44, 48, 36
   //  arr.splice(2,2); //10, 50, 48, 36
   // console.log(arr);
+
+                        // OBJECT//
+// let obj = {
+// id : 11,
+// name : "react"
+// };
+  //  console.log(obj.id,obj["name"]); // {id:11, name:react}
     return (
     <div className="App">
       <header className="App-header">
@@ -30,7 +37,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org" 
           target="_blank"
           rel="noopener noreferrer"
         >
